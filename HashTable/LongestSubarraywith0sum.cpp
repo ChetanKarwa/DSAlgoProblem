dd635ee2bedaf2e18d613d4add78b6d039522442
unknown <cbkarwa@example.com>
2020-07-04 03:07:21 +0530

    problem 5

---
+++ HashTable/LongestSubarraywith0sum.cpp
@@ -0,0 +1,27 @@
+// Links : https://www.geeksforgeeks.org/find-the-largest-subarray-with-0-sum/
+
+/*
+Given an array of integers, find the length of the longest sub-array with sum equals to 0.
+
+*/
+
+int maxLen(int arr[], int n)
+{
+    unordered_map<int, int> presum;
+
+    int sum = 0;
+    int max_len = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+
+        if (arr[i] == 0 && max_len == 0)
+            max_len = 1;
+        if (sum == 0)
+            max_len = i + 1;
+        if (presum.find(sum) != presum.end()) {
+            max_len = max(max_len, i - presum[sum]);
+        }
+        else {
+            presum[sum] = i;
+        }
+    }
