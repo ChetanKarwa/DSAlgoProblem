6a44e089fe08c8010e65c986b472707959de3304
unknown <cbkarwa@example.com>
2020-07-05 01:25:28 +0530

    problem 1

---
+++ LinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,28 @@
+// LINK :
+
+/*
+
+Ques : Reverse a singly linked list.
+
+
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
+*/
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prevHead = nullptr;
+    while(head!=nullptr)
+    {
+        ListNode* nextHead = head->next;
+        head->next = prevHead;
+        prevHead  = head;
+        head = nextHead;
+    }
+    return prevHead;
+}
